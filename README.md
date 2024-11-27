7a518910e1dc8798082c390004eb2fddb2d15e7d
PabloApa7 <89990249+Pablooxx@users.noreply.github.com>
2024-11-27 16:35:39 -0300

    Mi repo final

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
-# Hola Mundo
+# .
 
-mi primer repositorio en github
+This template should help get you started developing with Vue 3 in Vite.
 
-i love :icecream:, :pizza:, :dog:
+## Recommended IDE Setup
 
-i love :star:, :book:, :moon:
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
 
-i love :brazil:, and i love :money_with_wings:
+## Customize configuration
 
-eu sou do :argentina:
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/components/DatosPersonales.vue
@@ -0,0 +1,84 @@
+<script setup>
+import linkedin from '/src/assets/linkedin_icon.svg';
+import instagram from '/src/assets/instagram_icon.svg';
+import github from '/src/assets/github_icon.svg';
+import curriculum from '/src/assets/cv_resume_icon.svg';
+
+const title = 'Pablo Aparicio';
+const descripcion = 'Quality Assurance - UTN';
+const residencia = 'San Miguel de Tucumán, Tucumán, Argentina';
+const presentacion = 'Hola, Bienvenido a mi portafolio de proyectos. Soy un Quality Assurance que esta aprendiendo programación en la UTN.';
+const redesSociales = [
+  { id: 1, name: 'linkedin', src: linkedin, url: 'https://www.linkedin.com/in/pabloaparicio7/' },
+  { id: 2, name: 'instagram', src: instagram, url: 'https://www.instagram.com/pabloaparicio7/' },
+  { id: 3, name: 'github', src: github, url: 'https://github.com/Pablooxx' },
+  { id: 4, name: 'curriculum', src: curriculum, url: 'https://drive.google.com/file/d/10yfv3TUwjpByLJ4ZPw7CKS6NDO_dbvvh/view?usp=sharing' }
+];
+const telefono = '+54 9 381-6173497';
+
+</script>
+
+<template>
+<section class="datos-personales">
+  <div class="card">
+    <h1>{{ title }}</h1>
+    <h2>{{ descripcion }}</h2>
+    <p>{{ presentacion }}</p>
+    <ul class="container-lista">
+      <li v-for="red in redesSociales" :key="red.id">
+        <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+      </li>
+    </ul>
+    <h3>📞 Mi Teléfono personal: {{ telefono }}</h3>
+    <h4>{{ residencia }}</h4>
+  </div>
+</section>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+}
+
+p {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+.card {
+  background-color: rgb(61, 61, 61);
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+}
+
+.container-lista {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+
+.icon-redsocial {
+  align-items: center;
+  background-color: aliceblue;
+  border-radius: 50%;
+  padding: 2px;
+  margin: 5px;
+  box-shadow: 0 0 5px rgba(219, 219, 219, 0.934);
+}
+
+.icon-redsocial:hover {
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+</style>
